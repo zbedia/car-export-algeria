0f0d093097476238a5a1ed8761e808d73bed1780
zbedia <zoheir.bedia@gmail.com>
2026-09-05 22:51:01 +0200

    Show prices in French locale (EUR symbol after amount) and fix CarXExport pricing

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,10 +1,16 @@
+import { registerLocaleData } from '@angular/common';
+import localeFr from '@angular/common/locales/fr';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { LOCALE_ID } from '@angular/core';
 import { AppComponent } from './app/app.component';
 import { errorInterceptor } from './app/interceptors/error.interceptor';
 
+registerLocaleData(localeFr);
+
 bootstrapApplication(AppComponent, {
   providers: [
-    provideHttpClient(withInterceptors([errorInterceptor]))
+    provideHttpClient(withInterceptors([errorInterceptor])),
+    { provide: LOCALE_ID, useValue: 'fr-FR' }
   ]
 }).catch((err) => console.error(err));
