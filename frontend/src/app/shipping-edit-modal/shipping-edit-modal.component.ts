d7b87385e0d6859ddc1c02f3d79859bf0bcb4798
zbedia <zoheir.bedia@gmail.com>
2026-09-07 23:43:11 +0200

    Make the shipping edit modal an accessible dialog with Escape handling and initial focus

--- frontend/src/app/shipping-edit-modal/shipping-edit-modal.component.ts
+++ frontend/src/app/shipping-edit-modal/shipping-edit-modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, EventEmitter, HostListener, Input, Output, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { TranslatePipe } from '../pipes/translate.pipe';
@@ -17,11 +17,23 @@ export interface ShippingEditResult {
   templateUrl: './shipping-edit-modal.component.html',
   styleUrls: ['./shipping-edit-modal.component.css']
 })
-export class ShippingEditModalComponent {
+export class ShippingEditModalComponent implements AfterViewInit {
   @Input() originPort: OriginPort = 'MARSEILLE';
   @Input() destinationPort: DestinationPort = 'ALGER';
   @Output() save = new EventEmitter<ShippingEditResult>();
   @Output() cancel = new EventEmitter<void>();
+
+  @ViewChild('cancelButton') private cancelButton?: ElementRef<HTMLButtonElement>;
+
+  ngAfterViewInit(): void {
+    this.cancelButton?.nativeElement.focus();
+  }
+
+  // Escape closes the dialog the same way the backdrop click and Cancel do.
+  @HostListener('document:keydown.escape')
+  onEscape(): void {
+    this.onCancel();
+  }
 
   onSave(): void {
     this.save.emit({ originPort: this.originPort, destinationPort: this.destinationPort });
